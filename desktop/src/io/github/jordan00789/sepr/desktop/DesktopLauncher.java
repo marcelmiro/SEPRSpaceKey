f0a5b4bf86e130279ac0c15f980f39f73534411d
rmh563@york.ac.uk <Hilldigger1#>
2020-01-26 16:28:06 +0000

    Window cannot be resized

--- desktop/src/io/github/jordan00789/sepr/desktop/DesktopLauncher.java
+++ desktop/src/io/github/jordan00789/sepr/desktop/DesktopLauncher.java
@@ -11,6 +11,7 @@ public class DesktopLauncher {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setTitle("Kroy");
         config.setWindowedMode(1280, 720);
+        config.setResizable(false);
         new Lwjgl3Application(new Kroy(), config);
     }
 }
