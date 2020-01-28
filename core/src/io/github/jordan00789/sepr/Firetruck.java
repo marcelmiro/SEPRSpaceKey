e857a649119594db4e17de5a71c030c1890415c0
AlmightyWishPig <rmh563@york.ac.uk>
2020-01-28 11:16:33 +0000

    Changed turn speed

--- core/src/io/github/jordan00789/sepr/Firetruck.java
+++ core/src/io/github/jordan00789/sepr/Firetruck.java
@@ -136,14 +136,6 @@ public class Firetruck extends Entity implements Attack, Moveable {
 	 * @return Turning speed
 	 */
 	private float getTurnSpeed() {
-
-		/**
-		if (velocity < 50f) {
-			return (200f / velocity);
-		} else {
-			return 8f;
-		}
-		 **/
 		return 4f;
 	}
 
